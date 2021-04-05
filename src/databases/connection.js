1fea52ea679e66d4528542a7af11b5ab8c8cddf0
rgimenesrodrigues <rodrigogimenesrodrigues@gmail.com>
2021-04-05 00:22:38 -0300

    Ajustando Conexao

--- src/databases/connection.js
+++ src/databases/connection.js
@@ -2,10 +2,10 @@ var knex = require('knex')({
     client: 'mysql',
 
     connection: {
-        host : 'localhost',
-        user : 'root',
-        password : '',
-        database : 'pucmgpoc'
+        host : 'xlf3ljx3beaucz9x.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+        user : 'ezbnjw1aqcdla55w',
+        password : 'mhh7z31u6dn7o0oh',
+        database : 'wa0dzvw2zzqa2wdc'
      }
 });
 
