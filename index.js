833e5d0d88c6eb10584647a20126d6433083a661
rgimenesrodrigues <rodrigogimenesrodrigues@gmail.com>
2021-04-05 00:34:17 -0300

    Ajustando porta

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const express = require('express')
 const cors = require('cors')
 const router = require('./src/routes/routes')
-
+const PORT = process.env.PORT || '8080'
 
 const app = express()
 app.use(cors());
 app.use(express.json())
 app.use(router)
 
-app.listen(4000,()=>{
-    console.log("Aplicação rodando na porta 400")
+app.set("port",PORT)
+app.listen(PORT,()=>{
+    console.log("Aplicação rodando na porta "+ PORT)
 })
 app.get('/v1/inserirNorma',(request,response)=>{
     response.send("Inseriu a norma")
