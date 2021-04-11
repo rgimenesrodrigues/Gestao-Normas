9b23d2c8f20c836734b28120d0d2acdc841de6b7
rgimenesrodrigues <rodrigogimenesrodrigues@gmail.com>
2021-04-11 12:25:10 -0300

    Segurança

--- src/controllers/normasController.js
+++ src/controllers/normasController.js
@@ -1,7 +1,12 @@
 const database = require('../databases/connection')
 const jwt = require('jsonwebtoken');
 class normasController{
+
     listarNormas(request,response,next){
+        if (!request.header('apiKey') || request.header('apiKey') !== process.env.API_KEY) {
+            return response.status(401).json({status: 'Erro de Autenticação', message: 'Não atutorizado.'})
+        }
+
         database.select("*").table("normas").then(normas=>{ 
            response.status(200).json({normas})
         }).catch(error=>{
@@ -11,6 +16,11 @@ class normasController{
 
     }
     listarNorma(request,response){
+        
+        if (!request.header('apiKey') || request.header('apiKey') !== process.env.API_KEY) {
+            return response.status(401).json({status: 'Erro de Autenticação', message: 'Não atutorizado.'})
+        }
+
         const p_codigo  =  request.params.codigo
         database.select("*").table("normas").where({codigo:p_codigo}).then(norma=>{ 
             console.log(norma.length)
