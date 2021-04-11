b8ae0cbb7821ae4ab9b922933a192ea3c12abdb8
rgimenesrodrigues <rodrigogimenesrodrigues@gmail.com>
2021-04-11 13:49:39 -0300

    Ajust

--- index.js
+++ index.js
@@ -5,18 +5,12 @@ const router = require('./src/routes/routes')
 const PORT = process.env.PORT || '8080'
 const swaggerUi = require('swagger-ui-express')
 const swaggerFile = require('./src/routes/swagger-output.json')
-const helmet = require('helmet')
-const compression = require('compression')
-require('dotenv/config');
 
-const isProduction = process.env.NODE_ENV === 'production'
 
 const app = express()
 app.use(cors());
 app.use(express.json())
 app.use(router)
-app.use(compression())
-app.use(helmet())
 app.set("port",PORT)
 router.use('/api-docs', swaggerUi.serve);
 router.get('/api-docs', swaggerUi.setup(swaggerFile));
